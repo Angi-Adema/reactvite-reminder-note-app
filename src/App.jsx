0a59b0bf7ab537f50100f1f92ba15de72fd7d32c
CoderGirl <angi.adema@gmail.com>
2023-11-24 16:23:10 -0700

    updated css for screen size adaptability, updated fonts, colors and buttons for notes in css as well as added hard coded notes in App.jsx in order to test out the note functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,40 @@
+import { useState } from "react"
 import "./styles.css"
 
 export default function App() {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      title: "test note 1",
+      content: "bla bla note1",
+    },
+    {
+      id: 2,
+      title: "test note 2 ",
+      content: "bla bla note2",
+    },
+    {
+      id: 3,
+      title: "test note 3",
+      content: "bla bla note3",
+    },
+    {
+      id: 4,
+      title: "test note 4 ",
+      content: "bla bla note4",
+    },
+    {
+      id: 5,
+      title: "test note 5",
+      content: "bla bla note5",
+    },
+    {
+      id: 6,
+      title: "test note 6",
+      content: "bla bla note6",
+    },
+  ])
+
   return (
     <div className="main-container">
       <form className="form">
@@ -10,10 +44,16 @@ export default function App() {
         <button type="submit">Save Note</button>
       </form>
       <div className="notes-grid">
-        <div className="single-note">
-          <div className="note-header">
-            <button>Delete</button>
+        {notes.map((note) => (
+          <div className="note-item">
+            <div className="note-header">
+              <button>Delete</button>
+            </div>
+            <h2>{note.title}</h2>
+            <p>{note.content}</p>
           </div>
+        ))}
+        <div className="single-note">
           <h2>Note Title</h2>
           <p>Note Text</p>
         </div>
