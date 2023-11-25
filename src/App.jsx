16e1cb0526f5c71438a787bfec313b2333736232
CoderGirl <angi.adema@gmail.com>
2023-11-24 17:20:18 -0700

    updated the App.jsx with note functionality to be able to add notes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react"
 import "./styles.css"
 
 export default function App() {
+  const [title, setTitle] = useState("")
+  const [noteContent, setNoteContent] = useState("")
   const [notes, setNotes] = useState([
     {
       id: 1,
@@ -18,28 +20,41 @@ export default function App() {
       title: "test note 3",
       content: "bla bla note3",
     },
-    {
-      id: 4,
-      title: "test note 4 ",
-      content: "bla bla note4",
-    },
-    {
-      id: 5,
-      title: "test note 5",
-      content: "bla bla note5",
-    },
-    {
-      id: 6,
-      title: "test note 6",
-      content: "bla bla note6",
-    },
   ])
+
+  const handleSubmitNote = (e) => {
+    e.preventDefault()
+
+    const newNote = {
+      id: notes.length + 1,
+      title: title,
+      noteContent: noteContent,
+    }
+
+    console.log("noteContent: ", noteContent)
+
+    setNotes([newNote, ...notes])
+
+    setTitle("")
+    setNoteContent("")
+  }
 
   return (
     <div className="main-container">
-      <form className="form">
-        <input type="text" placeholder="Title" required />
-        <textarea type="text" placeholder="Enter Note" rows={15} required />
+      <form className="form" onSubmit={handleSubmitNote}>
+        <input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Title"
+          required
+        />
+        <textarea
+          value={noteContent}
+          onChange={(e) => setNoteContent(e.target.value)}
+          placeholder="Enter Note"
+          rows={15}
+          required
+        />
 
         <button type="submit">Save Note</button>
       </form>
@@ -50,13 +65,9 @@ export default function App() {
               <button>Delete</button>
             </div>
             <h2>{note.title}</h2>
-            <p>{note.content}</p>
+            <p>{note.noteContent}</p>
           </div>
         ))}
-        <div className="single-note">
-          <h2>Note Title</h2>
-          <p>Note Text</p>
-        </div>
       </div>
     </div>
   )
