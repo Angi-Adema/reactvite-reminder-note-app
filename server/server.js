89130fc08be14c6a75233fcf408c47aec5f3dd8c
CoderGirl <angi.adema@gmail.com>
2023-11-27 15:30:13 -0700

    added server folder

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const path = require("path")
+const db = require("./config/connection")
+
+const app = express()
+const PORT = process.env.PORT || 3001
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")))
+}
+
+db.once("open", () => {
+  app.listen(PORT, () => console.log("server is running"))
+})
