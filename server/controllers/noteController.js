be1c7d61ec8528c45bdef05b09eb3d7ae624dae3
CoderGirl <angi.adema@gmail.com>
2023-11-29 14:40:23 -0700

    added code the models index.js and Note.js to complete the model, also added the noteController.js with code to begin the routes

---
+++ server/controllers/noteController.js
@@ -0,0 +1,37 @@
+const { Note } = require("../models")
+
+module.exports = {
+  getNotes(req, res) {
+    Note.find()
+      .then((notes) => res.json(notes))
+      .catch((err) => res.status(500).json(err))
+  },
+  //   createNote(req, res) {
+  //     Note.create(req.body)
+  //       .then((dbNoteData) => res.json(dbNoteData))
+  //       .catch((err) => res.status(500).json(err))
+  //   },
+  //   updateNote(req, res) {
+  //     Note.findOneAndUpdate(
+  //       { _id: req.params.noteId },
+  //       { $set: req.body },
+  //       { runValidators: true, new: true }
+  //     )
+  //       .then((note) =>
+  //         !note
+  //           ? res.status(404).json({ message: "No note with this ID!" })
+  //           : res.json(note)
+  //       )
+  //       .catch((err) => {
+  //         console.log(err)
+  //         res.status(500).json(err)
+  //       })
+  //   },
+  //   deleteNote(req, res) {
+  //     Note.findOneAndRemove({ _id: req.params.noteId })
+  //     .then((note) =>
+  //     !note
+  //     ? res.status(404).json({ message: 'No note with this ID!' })
+  //     : )
+  //   },
+}
