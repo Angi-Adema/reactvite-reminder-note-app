c3ce62a610911def0d2b92a8995d3063535dad7b
CoderGirl <angi.adema@gmail.com>
2023-11-29 15:03:13 -0700

    updated noteRoutes.js with routes needed for the notes app

---
+++ server/routes/api/noteRoutes.js
@@ -0,0 +1,14 @@
+const router = require("express").Router()
+
+const {
+  getNotes,
+  createNote,
+  updateNote,
+  deleteNote,
+} = require("../../controllers/noteController")
+
+router.route("/").get(getNotes).post(createNote)
+
+router.route("/:noteId").put(updateNote).delete(deleteNote)
+
+module.exports = router
