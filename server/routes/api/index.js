dbd75493a6a9f5c849ec3e3111bcbe40014a9426
CoderGirl <angi.adema@gmail.com>
2023-12-07 08:06:49 -0700

    added userRoutes to routes api index.js file, added userRoutes.js to routes api and userController.js to the controllers

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router()
 const noteRoutes = require("./noteRoutes")
+const userRoutes = require("./userRoutes")
 
 router.use("/notes", noteRoutes)
+router.use("/users", userRoutes)
 
 module.exports = router
