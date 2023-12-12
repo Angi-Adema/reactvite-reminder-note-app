6d52b96ed138dd337e8bcba54234e95c8f391edc
CoderGirl <angi.adema@gmail.com>
2023-12-12 15:05:10 -0700

    updated components files for the home, login and NoteForm pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,36 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
 import App from "./App.jsx"
+import Notes from "./pages/NoteForm"
+import Login from "./pages/Login"
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./styles.css"
+import Home from "./pages/Home/index.jsx"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/notes",
+        element: <Notes />,
+      },
+    ],
+  },
+])
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 )
---
+++ client/src/pages/NoteForm/index.jsx
@@ -0,0 +1,152 @@
+import { useEffect, useState } from "react"
+
+import { getAllNotes, deleteNote, addNote, editNote } from "../../utils/API"
+
+export default function Notes() {
+  const [title, setTitle] = useState("")
+  const [noteContent, setNoteContent] = useState({
+    noteTitle: "",
+    noteBody: "",
+  })
+  const [selectedNote, setSelectedNote] = useState(false)
+  const [notes, setNotes] = useState([])
+  const [savedId, setSavedId] = useState("")
+
+  useEffect(() => {
+    const noteData = async () => {
+      const response = await getAllNotes()
+
+      const data = await response.json()
+
+      setNotes(data)
+    }
+
+    noteData()
+  }, [notes])
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setNoteContent({ ...noteContent, [name]: value })
+  }
+
+  const handleSubmitNote = async (e) => {
+    e.preventDefault()
+
+    try {
+      const response = await addNote(noteContent)
+      if (!response.ok) {
+        throw new Error("something wrong")
+      }
+
+      setNoteContent({
+        noteTitle: "",
+        noteBody: "",
+      })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const handleNoteClick = (note) => {
+    setSelectedNote(true)
+    setNoteContent({
+      noteTitle: note.noteTitle,
+      noteBody: note.noteBody,
+    })
+    setSavedId(note._id)
+  }
+
+  const handleEditNote = async (e) => {
+    e.preventDefault()
+
+    try {
+      const response = await editNote(savedId, noteContent)
+
+      if (!response.ok) {
+        throw new Error("something wrong")
+      }
+
+      setSelectedNote(false)
+      setNoteContent({
+        noteTitle: "",
+        noteBody: "",
+      })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const handleCancel = () => {
+    setSelectedNote(false)
+    setNoteContent({
+      noteTitle: "",
+      noteBody: "",
+    })
+  }
+
+  const handleDelete = async (noteId) => {
+    try {
+      const del = await deleteNote(noteId)
+      setSelectedNote(false)
+      setNoteContent({
+        noteTitle: "",
+        noteBody: "",
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  return (
+    <div className="main-container">
+      <form
+        className="form"
+        onSubmit={(e) =>
+          selectedNote ? handleEditNote(e) : handleSubmitNote(e)
+        }
+      >
+        <input
+          value={noteContent.noteTitle}
+          name="noteTitle"
+          onChange={handleChange}
+          placeholder="Title"
+          required
+        />
+        <textarea
+          value={noteContent.noteBody}
+          onChange={handleChange}
+          name="noteBody"
+          placeholder="Enter Note"
+          rows={15}
+          required
+        />
+
+        {selectedNote ? (
+          <div className="edit-button">
+            <button type="submit">Save</button>
+            <button type="submit" onClick={handleCancel}>
+              Cancel
+            </button>
+          </div>
+        ) : (
+          <button type="submit">Save Note</button>
+        )}
+      </form>
+      <div className="notes-grid">
+        {notes?.map((note, i) => (
+          <div
+            key={i}
+            className="note-item"
+            onClick={() => handleNoteClick(note)}
+          >
+            <div className="note-header">
+              <button onClick={(e) => handleDelete(note._id)}>Delete</button>
+              {/* <button onClick={() => handleNoteClick(note)}>Edit</button> */}
+            </div>
+            <h2>{note.noteTitle}</h2>
+            <p>{note.noteBody}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
