5d15107b7c8b8995ce2bb1566dd2856e6848a007
CoderGirl <angi.adema@gmail.com>
2023-12-18 11:58:52 -0700

    styled the navbar

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -1,45 +1,44 @@
 import Container from "react-bootstrap/Container"
 import Nav from "react-bootstrap/Nav"
 import Navbar from "react-bootstrap/Navbar"
-import NavDropdown from "react-bootstrap/NavDropdown"
 // import Auth from "../../utils/auth"
 
 export default function Navigation() {
   const boolean = true
+
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+    <Navbar
+      expand="lg"
+      className="bg-body-tertiary"
+      style={{
+        position: "fixed",
+        top: "0",
+        left: "0",
+        width: "100%",
+        display: "flex",
+        justifyContent: "space-between",
+        paddingRight: "1rem",
+        paddingLeft: "1rem",
+      }}
+    >
+      <div>
+        <Navbar.Brand>
+          <span style={{ fontWeight: "bold" }}>Reminder Notes</span>
+        </Navbar.Brand>
+      </div>
+      <div>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav>
             <Nav.Link href="/">Home</Nav.Link>
             {boolean ? (
-              <Nav.Link href="#">logout</Nav.Link>
+              <Nav.Link href="#">Logout</Nav.Link>
             ) : (
               <Nav.Link href="/login">Login</Nav.Link>
             )}
-
-            {boolean ? (
-              <NavDropdown title="My Apps" id="basic-nav-dropdown">
-                <NavDropdown.Item href="/notes">Notes</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            ) : (
-              ""
-            )}
           </Nav>
         </Navbar.Collapse>
-      </Container>
+      </div>
     </Navbar>
   )
 }
