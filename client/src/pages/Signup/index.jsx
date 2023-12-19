b3d8a9de0e0a4ed6c0883e62c82ff74c0e147382
CoderGirl <angi.adema@gmail.com>
2023-12-19 09:42:53 -0700

    updated the user.js model to include first and last name, updated front end signup to remove the confirm password

--- client/src/pages/Signup/index.jsx
+++ client/src/pages/Signup/index.jsx
@@ -59,12 +59,6 @@ export default function Signup() {
             <Form.Control type="password" placeholder="Password" />
           </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label style={{ fontWeight: "bold" }}>
-              Confirm Password
-            </Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
           <Button
             variant="primary"
             type="submit"
