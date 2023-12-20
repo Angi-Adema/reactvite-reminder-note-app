2b462bb6c743498962101ae81d2f1af50b1d8a67
CoderGirl <angi.adema@gmail.com>
2023-12-20 14:31:26 -0700

    added npmrc file

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,8 @@ import react from "@vitejs/plugin-react-swc"
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 3000,
+    open: true,
     proxy: {
       "/api": {
         target: "http://localhost:3001",
