6535927348c7d2c47ba09dcdb4ed21a9aa2a9462
CoderGirl <angi.adema@gmail.com>
2023-12-20 14:38:13 -0700

    fix

--- server/server.js
+++ server/server.js
@@ -11,7 +11,11 @@ app.use(express.urlencoded({ extended: false }))
 app.use(routes)
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")))
+  app.use(express.static(path.join(__dirname, "../client/dist")))
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"))
+  })
 }
 
 db.once("open", () => {
